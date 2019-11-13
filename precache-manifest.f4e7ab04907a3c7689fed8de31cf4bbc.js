cf1cb9fc3c34a25d5e0a6d6af72fe811b1714ce0
Goran Ljaljić <gljaljic@gmail.com>
2019-11-13 11:05:18 +0100

    Updates

---
+++ precache-manifest.f4e7ab04907a3c7689fed8de31cf4bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73269d4d05e5a6ad6218c02e08735f7",
+    "url": "/yomdb/index.html"
+  },
+  {
+    "revision": "741acce2031b9d04e5dd",
+    "url": "/yomdb/static/css/main.12595812.chunk.css"
+  },
+  {
+    "revision": "56db8b29440a93894da7",
+    "url": "/yomdb/static/js/2.e5fee2b9.chunk.js"
+  },
+  {
+    "revision": "741acce2031b9d04e5dd",
+    "url": "/yomdb/static/js/main.1c929b3a.chunk.js"
+  },
+  {
+    "revision": "4d2dcad95235b668f93a",
+    "url": "/yomdb/static/js/runtime-main.8a060973.js"
+  }
+]);
